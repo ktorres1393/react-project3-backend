7a411e408e446d81f8056c7ea39b9e9e612bc8c8
Kassandra Torres <ktorres1393@gmail.com>
2020-12-08 13:24:07 -0500

    Adds checkAuth helper to scores router

--- routes/api/scores.js
+++ routes/api/scores.js
@@ -3,6 +3,17 @@ const router = express.Router();
 const scoresCtrl = require('../../controllers/scores');
 
 router.get('/', scoresCtrl.highScores);
-router.post('/', scoresCtrl.create);
+
+/*---------- Protected Routes ----------*/
+router.use(require("../../config/auth"));
+// adds check auth so if user is not sign in or token is not valis, gives "not auth " error
+
+router.post('/', checkAuth, scoresCtrl.create);
+
+//restrict access so ONLY valid authenticated users can access the route
+function checkAuth(req, res, next){
+    if(req.user) return next();
+    return res.status(401).json({msg: "Not Authorized"})
+}
 
 module.exports = router;
